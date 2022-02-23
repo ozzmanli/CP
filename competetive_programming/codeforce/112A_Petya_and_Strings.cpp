43a7161c844c61f49b353c44d7d732cc6bc51cae
Volkan Coskun <volkan.coskun@assaabloy.com>
2022-02-23 15:53:28 +0100

    formater 3

--- competetive_programming/codeforce/112A_Petya_and_Strings.cpp
+++ competetive_programming/codeforce/112A_Petya_and_Strings.cpp
@@ -20,25 +20,6 @@ int main()
     for (int i = 0; i < stringOne.length(); i++)
     {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         if (tolower(stringOne[i]) > tolower(stringTwo[i]))
         {
             cout << 1;
